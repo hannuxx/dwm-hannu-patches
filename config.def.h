7172bc1a11971630c3928bf985a3683d33e8c33b
Hannu Heikkinen <hannuxx@gmail.com>
2016-08-28 22:20:54 +0300

    Small update with enhanced colors

--- config.def.h
+++ config.def.h
@@ -2,20 +2,21 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_norm_fg[]     = "#bbbbbb";
+static const char col_norm_bg[]     = "#000000";
+static const char col_norm_bor[]    = "#cecece";
+static const char col_sel_fg[]      = "#000000";
+static const char col_sel_bg[]      = "#cfcfcf";
+static const char col_sel_bor[]     = "#000000";
 static const char *colors[SchemeLast][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel] =  { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { col_norm_fg, col_norm_bg, col_norm_bor },
+	[SchemeSel] =  { col_sel_fg, col_sel_bg,  col_sel_bor  },
 };
 
 /* tagging */
@@ -34,7 +35,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -56,7 +57,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_norm_fg, "-nf", col_norm_bg, "-sb", col_sel_bg, "-sf", col_sel_fg, NULL };
 static const char *termcmd[]  = { "st", NULL};
 static const char *term2cmd[]  = { "terminator", "-b", NULL, NULL};
 static const char *filemgrcmd[]  = { "rox-filer", NULL };
